7c315b3ae12fd351ef99d368e7599291d390b0fc
盖伦 <wanghongzhan@cai-inc.com>
2023-02-02 15:40:34 +0800

    first commit

---
+++ demo1/src/main/java/com/whz/jetty/LoginHttpServlet.java
@@ -0,0 +1,57 @@
+/**
+ *
+ * Copyright (c) 2014, Deem Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ */
+package com.whz.jetty;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@SuppressWarnings("serial")
+@WebServlet(urlPatterns = {"/login"})
+public class LoginHttpServlet extends HttpServlet {
+
+    /**
+     * 用户登录
+     *
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("接收到post请求，uri:" + request.getRequestURI());
+    }
+
+    /**
+     * 用户登录
+     *
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("接收到get请求，uri:" + request.getRequestURI());
+    }
+
+}
